9a62a63760c10b12cc7e8125c7d8bb410088ff1a
Vinay Yadav <vnydav.0009@gmail.com>
2024-07-15 20:04:29 +0530

    Day 2

---
+++ Day 2- Operators/index.js
@@ -0,0 +1,40 @@
+let a = 10,
+  b = 20;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+a += 7;
+b -= 10;
+
+// console.log(a)
+// console.log(b)
+
+// console.log(a>b)
+// console.log(a<b)
+// console.log(a==b)
+// console.log(a!=b)
+// console.log(a>=b)
+// console.log(a<=b)
+
+a = 10;
+b = " ";
+// console.log(a==b)
+// console.log(a!=b)
+// console.log(a===b)
+// console.log(a!==b)
+
+// console.log(a);
+// console.log(b);
+
+// console.log(a && b);
+// console.log(a || b);
+// console.log(!b);
+
+a = 20;
+b = 10;
+
+console.log(a > b ? "a is greater than b" : "b is greater than a");
