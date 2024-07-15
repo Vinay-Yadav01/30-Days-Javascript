c3f0bdf693f959cf965d0ef710743739c3ee1411
Vinay Yadav <vnydav.0009@gmail.com>
2024-07-15 20:04:12 +0530

    Day 1

---
+++ Day 1- Variables and Datatypes/index.js
@@ -0,0 +1,38 @@
+// var a = 10;
+// console.log(a);
+
+// console.log(a);
+// var a = 10;
+
+// let name = "vinay";
+// console.log(name);
+
+// const flag = true;
+// console.log(flag);
+
+const num = 10;
+const username = "vinay";
+const flag = true;
+const arr = [10, 20, 30];
+const obj = {
+  name: "vinay",
+  age: 21,
+};
+
+// console.log(typeof num);
+// console.log(typeof username);
+// console.log(typeof flag);
+// console.log(typeof arr);
+// console.log(typeof obj);
+
+let token = 7;
+console.log(token);
+
+token = 10;
+console.log(token);
+
+const thala = 7;
+console.log(thala);
+
+// thala = 10;        Cannot assign Another Value to const Variable because it is a constant.
+// console.log(thala);
