5b8e41371d13b19e774f79f3f0ab62adc52218ab
Vinay Yadav <vnydav.0009@gmail.com>
2024-07-16 18:10:13 +0530

    Day 4

---
+++ Day 4- Loops/index.js
@@ -0,0 +1,61 @@
+// for (let i = 1; i <=10 ; i++) {
+//     console.log(i);
+// }
+// let num = 5;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${num} x ${i} = ${5 * i}`);
+// }
+
+// let sum = 0;
+// let i = 1;
+// while(i<=10){
+//     sum = sum + i;
+//     i++;
+// }
+// console.log(sum);
+
+// let i = 10;
+// while(i>0){
+//     console.log(i);
+//     i--;
+// }
+
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
+
+// let fact = 1;
+// let i =1;
+// let num = 5;
+// do {
+//   fact *= i;
+//   i++;
+// } while (i <= num);
+
+// console.log(fact);
+
+// let num = 5;
+// let ans = "";
+// for (let i = 1; i <= num; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     ans+="*";
+//   }
+//   ans+="\n";
+// }
+// console.log(ans)
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 5) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 7) {
+//     break;
+//   }
+//   console.log(i);
+// }
