42bee4317cdf999798f45ad7d42a990dbd481f7a
Vinay Yadav <vnydav.0009@gmail.com>
2024-07-16 17:45:17 +0530

    Day 3

---
+++ Day 3- Control Structures/index.js
@@ -0,0 +1,85 @@
+// let number = 5;
+// if (number > 0) console.log("Number is positive");
+// else if (number === 0) console.log("Number is zero");
+// else console.log("Number is negative");
+
+// let age = 18;
+// if (age >= 18) console.log("You are eligible to vote");
+// else console.log("You are not eligible to vote");
+
+// let a = 80,
+//   b = 50,
+//   c = 30;
+
+// if (a > b) {
+//   if (a > c) console.log("a is greater");
+//   else console.log("c is greater");
+// } else {
+//   if (b > c) console.log("b is greater");
+//   else console.log("c is greater");
+// }
+
+// let week = 5;
+
+// switch (week) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Invalid day");
+// }
+
+// let marks = 80;
+
+// switch (marks) {
+//   case marks >= 90:
+//     console.log("Grade A");
+//     break;
+//   case marks >= 80:
+//     console.log("Grade B");
+//     break;
+//   case marks >= 70:
+//     console.log("Grade C");
+//     break;
+//   case marks >= 60:
+//     console.log("Grade D");
+//     break;
+//   case marks >= 50:
+//     console.log("Grade E");
+//     break;
+//   case marks >= 40:
+//     console.log("Grade F");
+//     break;
+//   default:
+//     console.log("Fail");
+// }
+
+// let number = 5;
+// (number%2===0)?console.log("Even"):console.log("Odd");
+
+// let year = 2020;
+// if(year%4===0){
+//   if(year%100===0){
+//     if(year%400===0) console.log("Leap year");
+//     else console.log("Not a leap year");
+//   }else console.log("Leap year");
+// }
+// else console.log("Not a leap year");
