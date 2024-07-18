77ee4eb563a4654d7c7feebf6e2e7b195cb9f60b
Vinay Yadav <vnydav.0009@gmail.com>
2024-07-18 16:47:29 +0530

    Day 5

---
+++ Day 6- Arrays/index.js
@@ -0,0 +1,62 @@
+const arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[4]);
+
+// arr.pop();
+// arr.push(6);
+// console.log(arr);
+
+// arr.shift();
+// arr.unshift(0);
+// console.log(arr);
+
+// console.log(arr);
+// let arr2 = arr.map((element) => {
+//   return element * 2;
+// });
+
+// console.log(arr2);
+
+// console.log(arr);
+// let arr3 = arr.filter((element) => {
+//   return element % 2 === 0;
+// });
+
+// console.log(arr3);
+
+// let sum = arr.reduce((prev, element) => {
+//   return prev + element;
+// }, (prev = 0));
+// console.log(sum);
+
+// for (let index = 0; index < arr.length; index++) {
+//   console.log(arr[index]);
+// }
+
+// arr.forEach((element) => {
+//   console.log(element);
+// });
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+// console.log(matrix[2]); //Return 3rd Row
+// console.log(matrix[2][2]); //Return 3rd Row 3rd Column Element
+
+// for (let row = 0; row < matrix.length; row++) {
+//   for (let col = 0; col < matrix[0].length; col++) {
+//     console.log(matrix[row][col]);
+//   }
+// }
+
+// matrix.forEach((row) => {
+//   //For Each Row
+//   row.forEach((col) => {
+//     //For Each Column
+//     console.log(col);
+//   });
+// });
