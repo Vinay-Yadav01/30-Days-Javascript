374b304ca49e365c7d7064c248d45c91eaf7ac08
Vinay Yadav <vnydav.0009@gmail.com>
2024-07-25 12:15:27 +0530

    Day 13

---
+++ Day 13-Modules/main.js
@@ -0,0 +1,29 @@
+// import { add } from './modules/add.js';
+// import {user} from "./modules/user.js";
+// import {add, subtract, multiply, divide} from "./modules/calculator.js";
+// import hello from "./modules/hello.js";
+// import calculator from "./modules/calculator.js";
+// var _ = require("lodash");
+// import axios from 'axios';
+
+// console.log(add(1, 2)); // 3
+// console.log(user.name); // John
+// user.greet(); // Hello, my name is John and I am 30 years old.
+
+// console.log(add(1, 2)); // 3
+// console.log(subtract(2, 1)); // 1
+// console.log(multiply(2, 3)); // 6
+// console.log(divide(6, 3)); // 2
+
+// hello(); // Hello, world!
+
+// console.log(calculator.add(1, 2)); // 3
+// console.log(calculator.subtract(2, 1)); // 1
+// console.log(calculator.multiply(2, 3)); // 6
+// console.log(calculator.divide(6, 3)); // 2
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(_.sum(arr)); // 15
+// console.log(_.mean(arr)); // 3
+
+// console.log(axios.isCancel('something'));
---
+++ Day 13-Modules/modules/user.js
@@ -0,0 +1,9 @@
+export const user = {
+  name: "John",
+  age: 30,
+  greet() {
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    );
+  },
+};
