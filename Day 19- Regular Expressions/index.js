eda1db1a55760426f7c2aec863feca57c221b991
Vinay Yadav <vnydav.0009@gmail.com>
2024-07-31 17:41:53 +0530

    Day 19

---
+++ Day 19- Regular Expressions/index.js
@@ -0,0 +1,50 @@
+// const text1 = "Javascript is a popular programming language. Many developers love Javascript.";
+// const regex1 = /Javascript/g;
+// const matches1 = text1.match(regex1);
+// console.log(matches1);  
+
+// const text2 = "My phone number is 123-456-7890 and my zip code is 90210.";
+// const regex2 = /\d/g;
+// const matches2 = text2.match(regex2);
+// console.log(matches2);  
+
+// const text3 = "Hello World! This is a Regular Expression Test.";
+// const regex3 = /\b[A-Z][a-z]*\b/g;
+// const matches3 = text3.match(regex3);
+// console.log(matches3);  
+
+
+// const text4 = "There are 15 apples and 20 oranges.";
+// const regex4 = /\d+/g;
+// const matches4 = text4.match(regex4);
+// console.log(matches4); 
+
+// const phone = "(123) 456-7890";
+// const regex5 = /\((\d{3})\)\s(\d{3})-(\d{4})/;
+// const matches5 = phone.match(regex5);
+// console.log(matches5);  
+
+// const email = "himanshu@example.com";
+// const regex6 = /([^@]+)@([^@]+)/;
+// const matches6 = email.match(regex6);
+// console.log(matches6); 
+
+// const text7 = "Javascript is fun. Javascript is powerful.";
+// const regex7 = /^\w+/;
+// const matches7 = text7.match(regex7);
+// console.log(matches7); 
+
+// const text8 = "Learn Javascript, use Javascript.";
+// const regex8 = /\w+$/;
+// const matches8 = text8.match(regex8);
+// console.log(matches8);  
+
+// const password = "Passw0rd!";
+// const regex9 = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+// const isValidPassword = regex9.test(password);
+// console.log(isValidPassword); 
+
+// const url = "https://www.example.com";
+// const regex10 = /^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]+)\.([a-zA-Z]{2,})(\/\S*)?$/;
+// const isValidURL = regex10.test(url);
+// console.log(isValidURL);  
