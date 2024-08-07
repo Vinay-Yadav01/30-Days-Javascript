4385798fdff157d8a37a174e31aac94d7f244158
Vinay Yadav <vnydav.0009@gmail.com>
2024-08-07 23:15:41 +0530

    Day 26

---
+++ Day 26- Chat Application/public/client.js
@@ -0,0 +1,48 @@
+const socket = io();
+
+let username;
+let textarea = document.querySelector("#textarea");
+let chatbox = document.querySelector(".chat-box");
+do {
+  username = prompt("Enter your username:");
+} while (!username);
+
+textarea.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    sendMessage(e.target.value);
+  }
+});
+
+function sendMessage(message) {
+  let msg = {
+    user: username,
+    message: message,
+  };
+
+  // Append
+  appendMessage(msg, "outgoing");
+  textarea.value = "";
+  scrollToBottom();
+
+  // Send to server
+  socket.emit("message", msg);
+}
+
+function appendMessage(msg, type) {
+  let mainDiv = document.createElement("div");
+  let className = type;
+  mainDiv.classList.add(className, "message");
+
+  let markup = `
+             <p class="meta">${msg.user}</p>
+                <p class="text">
+                    ${msg.message}
+                </p>
+    `;
+  mainDiv.innerHTML = markup;
+  chatbox.appendChild(mainDiv);
+}
+
+function scrollToBottom() {
+  chatbox.scrollTop = chatbox.scrollHeight;
+}
